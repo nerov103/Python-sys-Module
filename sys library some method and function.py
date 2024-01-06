79ef105975d8b8e6cb4cb7f6b2fc77528d98e6cb
nerov103 <nibir01000@gmail.com>
2024-01-07 00:53:25 +0600

    This is my fast code in sys library in python, and to day ima upload some sys (Method and function)

---
+++ sys library some method and function.py
@@ -0,0 +1,73 @@
+#module import
+import sys
+
+#Cheak The version
+# print(sys.version)
+
+# for line in sys.stdin:
+#     if 'q' == line.rstrip():
+#         print(f"Import: {line}", end="")
+#         break
+# print("exit")
+
+# some_text = "Hello World !     "
+
+# print(some_text.rsplit())
+
+
+  
+# sys.stdout.write('gfg') 
+# sys.stdout.write('geeks') 
+# sys.stdout.write('\n') 
+# sys.stdout.write('for geeks') 
+
+# var = sys.stdout
+# arr = ['geeks', 'for', 'geeks']
+
+# for i in arr:
+#     var.write(i)
+
+
+
+# for j in arr:
+#     var.write('\n'+j)
+
+
+# def stderr(*a):
+#     print(*a, file=sys.stderr)
+
+# stderr("Hello World !!")
+
+
+
+ 
+# print("Example 1")
+# print("Example 2", file=sys.stderr)
+# sys.stderr.write("Example 3")
+
+# n = len(sys.argv)
+# print("Total arguments passed:", n)
+# print("\nName of Python script:", sys.argv[0])
+# print("\nArguments passed:", end = " ")
+# for i in range(1, n):
+#     print(sys.argv[i], end = " ")
+# Sum = 0
+# for i in range(1, n):
+#     Sum += int(sys.argv[i])
+     
+# print("\n\nResult:", Sum)
+
+
+# aga = 170
+# if aga < 18:
+#     sys.exit("Age less than 18")
+# else:
+#     sys.stdout.write("Age is not less than 18 \n")
+
+
+print(sys.path)
+
+
+
+
+
