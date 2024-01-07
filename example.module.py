32477a31b0d0f47027995caa9d9551cdef4a7658
nerov103 <nibir01000@gmail.com>
2024-01-07 16:05:09 +0600

    to day ima uploadat some New method and i will show how to used this method

---
+++ example.module.py
@@ -0,0 +1,75 @@
+import sys
+
+# print('HEllo')
+# sys.exit()
+# print("not run")
+
+# print(sys.executable)
+# print(sys.copyright)
+
+
+# loc_path = sys.path
+# for i in loc_path:
+#     print(i)
+
+
+# platfrom = sys.platform
+# print(platfrom)
+
+# try:
+#     x = 1/0
+# except:
+#     tupl = sys.exc_info()
+#     for a in tupl:
+#         print(a)
+
+# Ver = sys.getdefaultencoding()
+# print(Ver)
+
+# a = 20
+
+# print(sys.getsizeof(a))
+# print(a.__sizeof__())
+
+# print(sys.getrefcount(a))
+# limite = sys.getrecursionlimit()
+# print(limite)
+
+
+# i = 0
+# lemit = 2000
+# try:
+#     sys.setrecursionlimit(lemit)
+#     def myfun():
+#         global i
+#         i = i+1
+#         # if i == 996:
+#             # sys.exit("Max Limet!")
+#         print("Hello World!",i)
+#         myfun()
+#     myfun()
+# except:
+#     for i in sys.exc_info():
+#         print(i)
+
+
+# import time
+
+# for i in range(1, 10):
+#     print(i, end=" ", flush=True)
+#     time.sleep(0.5)
+
+# for x in range(10000):
+#     print(x)
+#     sys.stdout.flush(True)
+
+# name = sys.stdin.readline() 
+# print(name) 
+  
+# num = sys.stdin.readline(2) 
+# print(num) 
+
+sys.path.append("/home/backbox/For BackBox/Sys/folder/")
+sys.path
+
+
